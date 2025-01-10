0888a4c0f2c1a419fc8a848ab33a2ad96b62a914
lola2607123 <123645230+lola2607123@users.noreply.github.com>
2025-01-10 12:32:27 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -95,8 +95,8 @@ with tab2:
     with col2:
         st.image("library.png")
 
-	st.subheader("Select one of the 72 Melakartha ragams")
-	option = st.selectbox(
+st.subheader("Select one of the 72 Melakartha ragams")
+option = st.selectbox(
 	    "",
 ("Kanakangi", "Ratnangi", "Ganamurti", "Vanaspati", "Manavati",  
 "Tanarupi", "Senavati", "Hanumatodi", "Dhenuka", "Natakapriya",  
