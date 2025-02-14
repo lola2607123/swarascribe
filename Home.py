e6bfee08910e85ddeadf537200b82f469335c871
vestrol <abhinav.rathi@athenaeducation.co>
2025-02-14 18:04:44 +0530

    Update Home.py

--- Home.py
+++ Home.py
@@ -140,7 +140,9 @@ if raag is not None:
         st.write('**Time of Day**: ' + raag["time_of_day"])
         st.write('**Character**: ' + raag["character"])
         st.write('**Associated Deity**: ' + raag["associated_deity_emotion"])
-        st.write('**Popular Compositions**: ' + raag["popular_compositions"])
+        st.write('**Popular Compositions**: ')
+	for comp in raag["popular_compositions"]:
+	     st.write(comp)
         st.write('**History**: ' + raag["history"])
         st.write('**Cultural Significance**: ' + raag["cultural_significance"])
 
